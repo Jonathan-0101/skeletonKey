0d8416f99402f1319b49f8d4e84dee6c3963e446
Jonathan-0101 <62551253+Jonathan-0101@users.noreply.github.com>
2025-03-07 12:18:01 +0000

    Deploying to gh-pages from @ Jonathan-0101/PenTestingDevice@e48b3b4ecd199f5bb564fb4759e61319f9322d13 🚀

--- annotated_dup.js
+++ annotated_dup.js
@@ -1,6 +1,8 @@
 var annotated_dup =
 [
     [ "HCCAPX", "db/d68/structHCCAPX.html", null ],
+    [ "IRCommand", "dc/dfd/structIRCommand.html", null ],
+    [ "IRDeviceInfo", "d7/d59/structIRDeviceInfo.html", null ],
     [ "IRTools", "da/d24/classIRTools.html", null ],
     [ "wifi_ieee80211_mac_hdr_t", "d6/d1f/structwifi__ieee80211__mac__hdr__t.html", null ],
     [ "wifi_ieee80211_packet_t", "d9/dda/structwifi__ieee80211__packet__t.html", null ],
--- search/all_7.js
+++ search/all_7.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['irtools_0',['IRTools',['../da/d24/classIRTools.html',1,'']]]
+  ['ircommand_0',['IRCommand',['../dc/dfd/structIRCommand.html',1,'']]],
+  ['irdeviceinfo_1',['IRDeviceInfo',['../d7/d59/structIRDeviceInfo.html',1,'']]],
+  ['irtools_2',['IRTools',['../da/d24/classIRTools.html',1,'']]]
 ];
